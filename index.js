55812ee81b8241e2b7eb533d0ab99c9140442531
leader22 <y.sugiura.0316@gmail.com>
2017-02-10 15:34:09 +0900

    make mmss singleton

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
+const os = require('os');
 const argv = require('yargs')
   .usage('Usage: $0 --mpath [path/to/music/dir] --port [num]')
   .demandOption(['mpath'])
   .alias('p', 'port')
   .default('port', 8080)
   .argv;
-console.log(`Starting app...\n  mpath: ${argv.mpath}\n  port:  ${argv.port}`);
 
-const Mmss = require('./lib/mmss');
+const mpath = argv.mpath.replace('~', os.homedir());
+const port = argv.port;
+
+console.log(`Starting app...\n  mpath: ${mpath}\n  port:  ${port}`);
+
+const mmss = require('./lib/mmss');
 const server = require('./lib/server');
 
-const mmss = new Mmss(argv.mpath);
-mmss.build()
+mmss
+  .init(mpath)
+  .build()
   .then(() => {
-    server.listen(argv.port, () => {
-      console.log(`App is running on port: ${argv.port}`);
+    server.listen(port, () => {
+      console.log(`App is running on port: ${port}`);
     });
   })
-  .catch((err) => {
+  .catch(err => {
     console.error(err);
     process.exit(1);
   });
