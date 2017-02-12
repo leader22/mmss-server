195d4b84a0796cf793c32944100e820631ccf9ef
leader22 <y.sugiura.0316@gmail.com>
2017-02-12 16:40:11 +0900

    pass user/pass from argv

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const os = require('os');
 const argv = require('yargs')
-  .usage('Usage: $0 --mpath [path/to/music/dir] --port [num]')
-  .demandOption(['mpath'])
-  .alias('p', 'port')
+  .usage('Usage: $0 --mpath [path/to/music/dir] --user [username] --pass [password] --port [num]')
+  .demandOption(['mpath', 'user', 'pass'])
   .default('port', 8080)
+  .help()
   .argv;
-
-const mpath = argv.mpath.replace('~', os.homedir());
-const port = argv.port;
-
-console.log(`Starting app...\n  mpath: ${mpath}\n  port:  ${port}`);
-
 const mmss = require('./lib/mmss');
 const server = require('./lib/server');
 
+const mpath = argv.mpath.replace('~', os.homedir());
+const { port, user, pass }= argv;
+
+console.log(`Starting app...
+  mpath:     ${mpath}
+  port:      ${port}
+  user/pass: ${user}/${pass}
+`);
+
 mmss
-  .build(mpath)
+  .build(mpath, { user, pass })
   .then(() => {
     server.listen(port, () => {
       console.log(`App is running on port: ${port}`);
