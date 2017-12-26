cbba982ec2a01a8fa6a3c47e81435a0953ab97bd
Yuji Sugiura <y.sugiura.0316@gmail.com>
2017-12-27 08:21:12 +0900

    Refactor

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const os = require('os');
 const argv = require('yargs')
   .usage('Usage: $0 --mpath [path/to/music/dir] --user [username] --pass [password] --port [num]')
   .demandOption(['mpath', 'user', 'pass'])
@@ -8,8 +7,7 @@ const argv = require('yargs')
 const mmss = require('./lib/mmss');
 const server = require('./lib/server');
 
-const mpath = argv.mpath.replace('~', os.homedir());
-const { port, user, pass } = argv;
+const { mpath, port, user, pass } = argv;
 
 console.log(`Starting app...
   mpath:     ${mpath}
@@ -17,14 +15,14 @@ console.log(`Starting app...
   user/pass: ${user}/${pass}
 `);
 
-mmss
-  .build(mpath, { user, pass })
-  .then(() => {
-    server.listen(port, () => {
-      console.log(`App is running on port: ${port}`);
+(async () => {
+  await mmss.build(mpath, { user, pass })
+    .catch(err => {
+      console.error(err);
+      process.exit(1);
     });
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
+
+  server.listen(port, () => {
+    console.log(`App is running on port: ${port}`);
   });
+})();
